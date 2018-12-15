e1e43e266d5c3c95658c4e05d069ad3841c9341f
Alex Dovzhanyn <dovzhanyn.alex@gmail.com>
2018-12-14 19:38:40 -0500

    add more opcodes, start parsing type section

--- lib/decoder.ex
+++ lib/decoder.ex
@@ -1,6 +1,7 @@
 defmodule WaspVM.Decoder do
   alias WaspVM.LEB128
   alias WaspVM.Module
+  alias WaspVM.OpCodes
   require IEx
 
   def decode_file(file_path) do
@@ -32,9 +33,12 @@ defmodule WaspVM.Decoder do
   defp do_decode(module, bin) do
     <<section_code, rest::binary>> = bin
 
-    {section, rest} = decode_section(section_code, rest)
+    {unparsed_section, rest} = decode_section(section_code, rest)
 
-    module = Module.add_section(module, section_code, section)
+    module =
+      module
+      |> Module.add_section(section_code, unparsed_section)
+      |> parse_section(section_code)
 
     do_decode(module, rest)
   end
@@ -49,6 +53,25 @@ defmodule WaspVM.Decoder do
     <<section::bytes-size(size), rest::binary>> = rest
 
     {section, rest}
+  end
+
+  defp parse_section(module, 1) do
+    <<count::bytes-size(4), entries::binary>> = Map.get(module.sections, 1)
+
+    {count, remaining} = LEB128.decode(count)
+
+    entries = remaining <> entries
+
+    if count > 0 do
+      entries =
+        entries
+        |> String.split(OpCodes.type_to_opcode(:func))
+        |> Enum.filter(& &1 != "")
+
+      IEx.pry
+    else
+      []
+    end
   end
 
 end
--- lib/module.ex
+++ lib/module.ex
@@ -2,7 +2,8 @@ defmodule WaspVM.Module do
 
   defstruct sections: %{},
             magic: nil,
-            version: nil
+            version: nil,
+            types: nil
 
   def add_section(module, sec_code, section) do
     sections = Map.put(module.sections, sec_code, section)
