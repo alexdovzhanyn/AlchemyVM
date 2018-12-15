263f7543124e5a9b9db3e95010a988bf59889d15
fantypants <8563615@gmail.com>
2018-12-15 00:30:29 -0800

    Dissasembler started

---
+++ lib/dissasembler.ex
@@ -0,0 +1,11 @@
+defmodule WaspVM.Dissasembler do
+
+  @spec dissasemble(Binary, Integer, Map) :: Tuple
+  def disassemble(_bin, _addr, _dissasembled), do: {:error, "Disassembler should be called with an empty map"}
+  def dissasemble(bin, addr, dissasembled), do: instruction(bin, addr, [:start_block], dissasembled)
+
+
+  @spec instruction(Binary, Integer, List, Map) :: Tuple
+  defp instruction(bin, addr, stack, dissasembled), do: {:ok, ""}
+
+end
