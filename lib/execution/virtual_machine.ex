137e6c09af48ce6db302dbb08d81cd374df58490
fantypants <8563615@gmail.com>
2018-12-17 01:36:02 -0800

    Tidied Virtual Machine file

--- lib/execution/virtual_machine.ex
+++ lib/execution/virtual_machine.ex
@@ -10,23 +10,11 @@ defmodule WaspVM.VirtualMachine do
     end
 
     def init(args) do
-      {:ok, %{vm: %{}, locals: [], filename: args[:filename]}}
+      {:ok, %{vm: %{}, locals: []}}
     end
 
     def start_vm(args) do
       GenServer.cast(__MODULE__, {:start_vm, args})
-    end
-
-    def execute(args) do
-      GenServer.cast(__MODULE__, {:execute, args})
-    end
-
-    def update(args) do
-      GenServer.cast(__MODULE__, {:update, args})
-    end
-
-    def update_memory(args) do
-      GenServer.cast(__MODULE__, {:update_memory, args})
     end
 
     def run_vm(args) do
@@ -35,10 +23,6 @@ defmodule WaspVM.VirtualMachine do
 
     def fetch() do
       GenServer.call(__MODULE__, :fetch)
-    end
-
-    def get_used() do
-      GenServer.call(__MODULE__, :get_used)
     end
 
     def fetch_locals() do
@@ -55,19 +39,6 @@ defmodule WaspVM.VirtualMachine do
       {:reply, state.locals, state}
     end
 
-    def handle_call(:get_used, _from, state) do
-      Logger.info("Received Get Used Event")
-      used = WaspVM.Memory.get_end(state.vm.memory)
-
-      {:reply, used, state}
-    end
-
-    def handle_cast({:execute, args}, state) do
-      Logger.info("Received Execute event")
-
-
-      {:noreply, state}
-    end
 
     def handle_cast({:start_vm, args}, state) do
       Logger.info("Received Start event")
@@ -76,25 +47,9 @@ defmodule WaspVM.VirtualMachine do
       {:noreply, state}
     end
 
-    def handle_cast({:update_memory, args}, state) do
-      Logger.info("Received Update Memory event")
-      memory = WaspVM.Memory.put_at(state.vm.memory, args.index, args.value)
-      vm = Map.put(state.vm, :memory, memory)
-      state = Map.put(state, :vm, vm)
-      {:noreply, state}
-    end
-
     def handle_cast({:run_vm, args}, state) do
       Logger.info("Received Run event")
       state = Map.put(state, :locals, args)
-
-      {:noreply, state}
-    end
-
-    def handle_cast({:update, args}, state) do
-      Logger.info("Received Update event")
-      IO.inspect args
-
 
       {:noreply, state}
     end
