fd60a6fde5bca6d3089f71fdb004b2fad82e9e1a
Alex Dovzhanyn <dovzhanyn.alex@gmail.com>
2018-12-30 03:25:06 -0500

    make paralellism optional

--- lib/decoding/decoder.ex
+++ lib/decoding/decoder.ex
@@ -16,41 +16,52 @@ defmodule WaspVM.Decoder do
 
   @moduledoc false
 
-  def decode_file(file_path) do
+  def decode_file(file_path, parallel) do
     {:ok, bin} = File.read(file_path)
 
-    decode(bin)
+    decode(bin, parallel)
   end
 
   @doc false
-  def decode(bin) when is_binary(bin), do: split_sections(%Module{}, bin)
+  def decode(bin, parallel) when is_binary(bin), do: begin_split(%Module{}, bin, parallel)
 
   @doc false
-  def decode(bin) do
+  def decode(bin, _parallel) do
     {fun, arity} = __ENV__.function
     raise "Invalid data provided for #{fun}/#{arity}. Must be binary, got: #{inspect(bin)}"
   end
 
-  defp split_sections(module, <<>>), do: parallel_decode(module)
-
-  defp split_sections(module, bin) when module == %Module{} do
+  defp begin_split(module, bin, parallel) do
     <<magic::bytes-size(4), version::bytes-size(4), rest::binary>> = bin
 
     if magic != <<0, 97, 115, 109>> do
       raise "Malformed or incomplete binary"
     else
-      split_sections(%Module{magic: magic, version: version}, rest)
+      split_sections(%Module{magic: magic, version: version}, rest, parallel)
     end
   end
 
-  defp split_sections(module, bin) do
+  defp split_sections(module, <<>>, true), do: parallel_decode(module)
+  defp split_sections(module, <<>>, false), do: module
+
+  defp split_sections(module, bin, parallel) do
     <<section_code, rest::binary>> = bin
 
     {unparsed_section, rest} = split_section(section_code, rest)
 
     module = Module.add_section(module, section_code, unparsed_section)
 
-    split_sections(module, rest)
+    module =
+      if !parallel do
+        section = Map.get(module.sections, section_code)
+        {k, v} = parse_section({section_code, section})
+
+        Map.put(module, k, v)
+      else
+        module
+      end
+
+    split_sections(module, rest, parallel)
   end
 
   defp split_section(sec_code, bin) when sec_code >= 0 do
