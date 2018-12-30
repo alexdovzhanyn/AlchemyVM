0e5e644456093f22090a06f8c9b613c138b66097
Alex Dovzhanyn <dovzhanyn.alex@gmail.com>
2018-12-30 02:00:30 -0500

    parallel decoding properly

--- lib/decoding/decoder.ex
+++ lib/decoding/decoder.ex
@@ -23,7 +23,7 @@ defmodule WaspVM.Decoder do
   end
 
   @doc false
-  def decode(bin) when is_binary(bin), do: do_decode(%Module{}, bin)
+  def decode(bin) when is_binary(bin), do: split_sections(%Module{}, bin)
 
   @doc false
   def decode(bin) do
@@ -31,37 +31,41 @@ defmodule WaspVM.Decoder do
     raise "Invalid data provided for #{fun}/#{arity}. Must be binary, got: #{inspect(bin)}"
   end
 
-  defp do_decode(module, <<>>), do: module
+  defp split_sections(module, <<>>), do: parallel_decode(module)
 
-  defp do_decode(module, bin) when module == %Module{} do
+  defp split_sections(module, bin) when module == %Module{} do
     <<magic::bytes-size(4), version::bytes-size(4), rest::binary>> = bin
 
     if magic != <<0, 97, 115, 109>> do
       raise "Malformed or incomplete binary"
     else
-      do_decode(%Module{magic: magic, version: version}, rest)
+      split_sections(%Module{magic: magic, version: version}, rest)
     end
   end
 
-  defp do_decode(module, bin) do
+  defp split_sections(module, bin) do
     <<section_code, rest::binary>> = bin
 
-    {unparsed_section, rest} = decode_section(section_code, rest)
+    {unparsed_section, rest} = split_section(section_code, rest)
 
-    module =
-      module
-      |> Module.add_section(section_code, unparsed_section)
-      |> parse_section(section_code)
+    module = Module.add_section(module, section_code, unparsed_section)
 
-    do_decode(module, rest)
+    split_sections(module, rest)
   end
 
-  defp decode_section(sec_code, bin) when sec_code >= 0 do
+  defp split_section(sec_code, bin) when sec_code >= 0 do
     {size, rest} = LEB128.decode_unsigned(bin)
 
     <<section::bytes-size(size), rest::binary>> = rest
 
     {section, rest}
+  end
+
+  defp parallel_decode(module) do
+    module.sections
+    |> Enum.map(fn {k, _} -> Task.async(fn -> parse_section(module, k) end) end)
+    |> Enum.map(&Task.await/1)
+    |> Enum.reduce(& Map.merge(&2, &1))
   end
 
   defp parse_section(module, 0), do: CustomSectionParser.parse(module)
