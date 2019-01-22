1a626e28c0e7b0d63b52de8def8c3553d09996b8
Alex Dovzhanyn <dovzhanyn.alex@gmail.com>
2019-01-21 21:48:24 -0500

    fix tests, implement data segment memory loading

--- test/executor_test.exs
+++ test/executor_test.exs
@@ -2,7 +2,6 @@ defmodule WaspVM.ExecutorTest do
   use ExUnit.Case
   require Bitwise
   doctest WaspVM
-  alias Decimal, as: D
 
   #### Basic Int Numeric Operations
   test "32 bit integers with add properly" do
@@ -86,7 +85,7 @@ defmodule WaspVM.ExecutorTest do
     test "32 bit sint can rem properly" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/types.wasm")
-      {status, gas, res} = WaspVM.execute(pid, "i32__rem_s", [2, -5])
+      {:ok, _gas, res} = WaspVM.execute(pid, "i32__rem_s", [2, -5])
       answer = :math.pow(2, 32) + res
       assert Kernel.round(answer) == 4294967295
     end
@@ -157,8 +156,7 @@ defmodule WaspVM.ExecutorTest do
     test "32 bit int / ns can shl properly" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/types.wasm")
-
-      {:ok, gas, result} = WaspVM.execute(pid, "i32__shl", [3, -100])
+      {:ok, _gas, result} = WaspVM.execute(pid, "i32__shl", [3, -100])
 
       answer = :math.pow(2, 32) + result
 
@@ -168,20 +166,17 @@ defmodule WaspVM.ExecutorTest do
     test "64 bit int can shl properly" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/types.wasm")
-
-      {:ok, gas, result} = WaspVM.execute(pid, "i64__shl", [3, -100])
+      {:ok, _gas, result} = WaspVM.execute(pid, "i64__shl", [3, -100])
 
       answer = Bitwise.band(result, 0xFFFFFFFFFFFFFFFF)
 
       assert round(answer) == 18446744073709550816
     end
 
-
     test "32 bit sint can shr properly" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/types.wasm")
-
-      {:ok, gas, result} = WaspVM.execute(pid, "i32__shr_s", [3, -100])
+      {:ok, _gas, result} = WaspVM.execute(pid, "i32__shr_s", [3, -100])
 
       assert Bitwise.band(result, 0xFFFFFFFF) == 4294967283
     end
@@ -189,37 +184,30 @@ defmodule WaspVM.ExecutorTest do
     test "64 bit sint can shr properly" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/types.wasm")
-
-      {:ok, gas, result} = WaspVM.execute(pid, "i64__shr_s", [3, -100])
+      {:ok, _gas, result} = WaspVM.execute(pid, "i64__shr_s", [3, -100])
 
       assert Bitwise.band(result, 0xFFFFFFFFFFFFFFFF) == 18446744073709551603
     end
 
- # NW
+    # NW
     test "32 bit uint can shr properly" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/types.wasm")
-
-      {:ok, gas, result} = WaspVM.execute(pid, "i32__shr_u", [3, 100])
-      
-      assert result == 536870899
+      assert {:ok, _gas, 536870899} = WaspVM.execute(pid, "i32__shr_u", [3, 100])
     end
 
-#NW
+    # NW
     test "64 bit uint can shr properly" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/types.wasm")
 
-      {:ok, gas, result} = WaspVM.execute(pid, "i64__shr_u", [5, 6])
-
-      #assert result == 0
+      assert {:ok, _gas, 0} = WaspVM.execute(pid, "i64__shr_u", [5, 6])
     end
 
     test "32 bit uint can rotl properly" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/types.wasm")
-
-      {:ok, gas, result} = WaspVM.execute(pid, "i32__rotl", [-100, 3])
+      {:ok, _gas, result} = WaspVM.execute(pid, "i32__rotl", [-100, 3])
 
       answer = :math.pow(2, 32) + result
 
@@ -229,19 +217,14 @@ defmodule WaspVM.ExecutorTest do
     test "32 bit uint can rotr properly" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/types.wasm")
-      {:ok, gas, result} = WaspVM.execute(pid, "i32__rotr", [16, 2])
-
-      assert result == 4
+      assert {:ok, _gas, 4} = WaspVM.execute(pid, "i32__rotr", [16, 2])
     end
 
 
     test "Call instruction works properly" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/nested_func_call.wasm")
-
-      {:ok, gas, result} =  WaspVM.execute(pid, "nested_func_call", [0, 32])
-
-      assert result == -7367
+      assert {:ok, _gas, -7367} =  WaspVM.execute(pid, "nested_func_call", [0, 32])
     end
 
     ### End simnple BitWise Ops
@@ -502,8 +485,7 @@ defmodule WaspVM.ExecutorTest do
     test "if statement works" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/if_2.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "ifOne", [0])
-      assert answer == 1
+      assert {status, _gas, 1} = WaspVM.execute(pid, "ifOne", [0])
     end
 
     ### END PARAMTERIC TEST
@@ -512,7 +494,7 @@ defmodule WaspVM.ExecutorTest do
     test "32 Store 8 works correctly" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/memory.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "i32_store8", [])
+      {:ok, _gas, answer} = WaspVM.execute(pid, "i32_store8", [])
 
       answer =
         answer
@@ -528,7 +510,7 @@ defmodule WaspVM.ExecutorTest do
     test "64 Store 8 works correctly" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/memory.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "i64_store8", [])
+      {:ok, _gas, answer} = WaspVM.execute(pid, "i64_store8", [])
 
       answer =
         answer
@@ -544,7 +526,7 @@ defmodule WaspVM.ExecutorTest do
     test "32 Store 16 works correctly" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/memory.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "i32_store16", [])
+      {:ok, _gas, answer} = WaspVM.execute(pid, "i32_store16", [])
 
       answer =
         answer
@@ -560,7 +542,7 @@ defmodule WaspVM.ExecutorTest do
     test "64 Store 16 works correctly" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/memory.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "i64_store16", [])
+      {:ok, _gas, answer} = WaspVM.execute(pid, "i64_store16", [])
 
       answer =
         answer
@@ -576,7 +558,7 @@ defmodule WaspVM.ExecutorTest do
     test "64 Store 32 works correctly" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/memory.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "i64_store32", [])
+      {:ok, _gas, answer} = WaspVM.execute(pid, "i64_store32", [])
 
       answer =
         answer
@@ -592,49 +574,47 @@ defmodule WaspVM.ExecutorTest do
     test "32 Store works correctly" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/memory.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "i32_store", [])
+      {:ok, _gas, answer} = WaspVM.execute(pid, "i32_store", [])
       assert answer == 4294843840
     end
 
     test "32 load8_s works correctly" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/load.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "i32_load8_s", [])
-      assert answer == 4294967295
+      assert {:ok, _gas, 255} = WaspVM.execute(pid, "i32_load8_s", [])
     end
 
     test "32 load8_u works correctly" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/load.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "i32_load8_u", [])
+      {:ok, _gas, answer} = WaspVM.execute(pid, "i32_load8_u", [])
       assert answer == 255
     end
 
     test "32 load16_u works correctly" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/load.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "i32_load16_u", [])
+      {:ok, _gas, answer} = WaspVM.execute(pid, "i32_load16_u", [])
       assert answer == 65535
     end
 
     test "64 load8_u works correctly" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/load.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "i64_load8_u", [])
-      assert answer == 65535
+      assert {:ok, _gas, 255} = WaspVM.execute(pid, "i64_load8_u", [])
     end
 
     test "64 load16_u works correctly" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/load.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "i64_load16_u", [])
+      {:ok, _gas, answer} = WaspVM.execute(pid, "i64_load16_u", [])
       assert answer == 65535
     end
 
     test "64 load32_u works correctly" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/load.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "i64_load32_u", [])
+      {:ok, _gas, answer} = WaspVM.execute(pid, "i64_load32_u", [])
       assert answer == 4294967295
     end
 
@@ -642,29 +622,25 @@ defmodule WaspVM.ExecutorTest do
     test "32 load16_s works correctly" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/load.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "i32_load16_s", [])
-      assert answer == 4294967295
+      assert {:ok, _gas, 65535} = WaspVM.execute(pid, "i32_load16_s", [])
     end
 
     test "64 load8_s works correctly" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/load.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "i64_load8_s", [])
-      assert answer == 18446744073709551615
+      assert {:ok, _gas, 255} = WaspVM.execute(pid, "i64_load8_s", [])
     end
 
     test "64 load16_s works correctly" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/load.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "i64_load16_s", [])
-      assert answer == 18446744073709551615
+      assert {:ok, _gas, 65535} = WaspVM.execute(pid, "i64_load16_s", [])
     end
 
     test "64 load32_s works correctly" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/load.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "i64_load32_s", [])
-      assert answer == 18446744073709551615
+      assert {:ok, _gas, 4294967295} = WaspVM.execute(pid, "i64_load32_s", [])
     end
 
     ### End Memory Tests
@@ -673,21 +649,19 @@ defmodule WaspVM.ExecutorTest do
     test "32 wrap 64 works correctly" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/wrap.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "i32_wrap_i64", [])
-      assert answer == 4294967295
+      assert {:ok, _gas, 4294967295} = WaspVM.execute(pid, "i32_wrap_i64", [])
     end
 
     test "f32 trunc i32 U works correctly" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/wrap.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "i32_trunc_u_f32", [])
-      assert answer == 3000000000
+      assert {:ok, _gas, 3000000000} = WaspVM.execute(pid, "i32_trunc_u_f32", [])
     end
 
     test "f32 trunc i32 S works correctly" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/wrap.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "i32_trunc_s_f32", [])
+      {:ok, _gas, answer} = WaspVM.execute(pid, "i32_trunc_s_f32", [])
 
       answer = Bitwise.band(answer, 0xFFFFFFFF)
       assert answer == 4294967196
@@ -696,14 +670,14 @@ defmodule WaspVM.ExecutorTest do
     test "f64 trunc i32 U works correctly" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/wrap.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "i32_trunc_u_f64", [])
+      {:ok, _gas, answer} = WaspVM.execute(pid, "i32_trunc_u_f64", [])
       assert answer == 3000000000
     end
 
     test "f64 trunc i32 S works correctly" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/wrap.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "i32_trunc_s_f64", [])
+      {:ok, _gas, answer} = WaspVM.execute(pid, "i32_trunc_s_f64", [])
 
       answer = Bitwise.band(answer, 0xFFFFFFFF)
       assert answer == 4294967196
@@ -712,14 +686,13 @@ defmodule WaspVM.ExecutorTest do
     test "f32 trunc i64 U works correctly" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/wrap.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "i64_trunc_u_f32", [])
-      assert answer == 1
+      assert {:ok, _gas, 1} = WaspVM.execute(pid, "i64_trunc_u_f32", [])
     end
 
     test "f32 trunc i64 S works correctly" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/wrap.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "i64_trunc_s_f32", [])
+      {:ok, _gas, answer} = WaspVM.execute(pid, "i64_trunc_s_f32", [])
 
       answer = Bitwise.band(answer, 0xFFFFFFFF)
       assert answer == 1
@@ -728,14 +701,13 @@ defmodule WaspVM.ExecutorTest do
     test "f64 trunc i64 U works correctly" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/wrap.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "i64_trunc_u_f64", [])
-      assert answer == 1
+      assert {:ok, _gas, 1} = WaspVM.execute(pid, "i64_trunc_u_f64", [])
     end
 
     test "f64 trunc i64 S works correctly" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/wrap.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "i64_trunc_s_f64", [])
+      {:ok, _gas, answer} = WaspVM.execute(pid, "i64_trunc_s_f64", [])
 
       answer = Bitwise.band(answer, 0xFFFFFFFF)
       assert answer == 1
@@ -744,7 +716,7 @@ defmodule WaspVM.ExecutorTest do
     test "f32 i32 S Convert works" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/wrap_fixed.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "f32_convert_s_i32", [])
+      {:ok, _gas, answer} = WaspVM.execute(pid, "f32_convert_s_i32", [])
 
       assert answer == WaspVM.Executor.float_point_op(-1 * 1.000000)
     end
@@ -752,7 +724,7 @@ defmodule WaspVM.ExecutorTest do
     test "f32 i32 U Convert works" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/wrap_fixed.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "f32_convert_u_i32", [])
+      {:ok, _gas, answer} = WaspVM.execute(pid, "f32_convert_u_i32", [])
 
       assert answer == WaspVM.Executor.float_point_op(4294967295 * 1.000000)
     end
@@ -760,7 +732,7 @@ defmodule WaspVM.ExecutorTest do
     test "f32 i64 S Convert works" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/wrap_fixed.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "f32_convert_s_i64", [])
+      {:ok, _gas, answer} = WaspVM.execute(pid, "f32_convert_s_i64", [])
 
       assert answer == WaspVM.Executor.float_point_op(0 * 1.000000)
     end
@@ -768,7 +740,7 @@ defmodule WaspVM.ExecutorTest do
     test "f32 i64 U Convert works" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/wrap_fixed.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "f32_convert_u_i64", [])
+      {:ok, _gas, answer} = WaspVM.execute(pid, "f32_convert_u_i64", [])
 
       assert answer == WaspVM.Executor.float_point_op(0 * 1.000000)
     end
@@ -776,7 +748,7 @@ defmodule WaspVM.ExecutorTest do
     test "f64 i64 S Convert works" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/wrap_fixed.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "f32_convert_s_i64", [])
+      {:ok, _gas, answer} = WaspVM.execute(pid, "f32_convert_s_i64", [])
 
       assert answer == WaspVM.Executor.float_point_op(0 * 1.000000)
     end
@@ -784,7 +756,7 @@ defmodule WaspVM.ExecutorTest do
     test "f64 i64 U Convert works" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/wrap_fixed.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "f32_convert_u_i64", [])
+      {:ok, _gas, answer} = WaspVM.execute(pid, "f32_convert_u_i64", [])
 
       assert answer == WaspVM.Executor.float_point_op(0 * 1.000000)
     end
@@ -792,7 +764,7 @@ defmodule WaspVM.ExecutorTest do
     test "f64 i32 S Convert works" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/wrap_fixed.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "f32_convert_s_i32", [])
+      {:ok, _gas, answer} = WaspVM.execute(pid, "f32_convert_s_i32", [])
 
       assert answer == WaspVM.Executor.float_point_op(-1 * 1.000000)
     end
@@ -800,7 +772,7 @@ defmodule WaspVM.ExecutorTest do
     test "f64 i32 U Convert works" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/wrap_fixed.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "f32_convert_u_i32", [])
+      {:ok, _gas, answer} = WaspVM.execute(pid, "f32_convert_u_i32", [])
 
       assert answer == WaspVM.Executor.float_point_op(4294967295 * 1.000000)
     end
@@ -808,23 +780,19 @@ defmodule WaspVM.ExecutorTest do
     test "i64 extend i32 u works" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/wrap_fixed.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "i64_extend_u_i32", [])
-
-      assert answer == 4294967295
+      assert {:ok, _gas, 4294967295} = WaspVM.execute(pid, "i64_extend_u_i32", [])
     end
 
     test "i64 extend i32 s works" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/wrap_fixed.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "i64_extend_s_i32", [])
-
-      assert answer == 18446744073709551615
+      assert {:ok, _gas, 18446744073709551615} = WaspVM.execute(pid, "i64_extend_s_i32", [])
     end
 
     test "f32 demote f64 works" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/wrap_fixed.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "f32_demote_f64", [])
+      {:ok, _gas, answer} = WaspVM.execute(pid, "f32_demote_f64", [])
 
       assert answer == WaspVM.Executor.float_point_op(123456789 * 1.0)
     end
@@ -832,7 +800,7 @@ defmodule WaspVM.ExecutorTest do
     test "f64 promote f32 works" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/wrap_fixed.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "f64_demote_f32", [])
+      {:ok, _gas, answer} = WaspVM.execute(pid, "f64_demote_f32", [])
 
       assert answer == WaspVM.Executor.float_point_op(12345679 * 1.0)
     end
@@ -843,15 +811,13 @@ defmodule WaspVM.ExecutorTest do
     test "i64 eq true works" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/compare.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "i64_eq_true", [])
-      assert answer == 1
+      assert {:ok, _gas, 1} = WaspVM.execute(pid, "i64_eq_true", [])
     end
 
     test "i64 eq false works" do
       {:ok, pid} = WaspVM.start()
       WaspVM.load_file(pid, "test/fixtures/wasm/compare.wasm")
-      {status, gas, answer} = WaspVM.execute(pid, "i64_eq_false", [])
-      assert answer == 0
+      assert {:ok, _gas, 0} = WaspVM.execute(pid, "i64_eq_false", [])
     end
 
     ### END COMPARE
@@ -864,7 +830,7 @@ defmodule WaspVM.ExecutorTest do
     end
 
     test "gas instantiates with limit correctly" do
-      {:ok, pid} = WaspVM.start()
+      # {:ok, pid} = WaspVM.start()
       #WaspVM.load_file(pid, "test/fixtures/wasm/add_2.wasm")
       #{status, gas_cost, stack_value} =  WaspVM.execute(pid, "_Z3addPi", [4], [gas_limit: 1])
       #{status, gas_cost2, stack_value2} =  WaspVM.execute(pid, "_Z3addPi", [4], [gas_limit: 10])
@@ -873,8 +839,4 @@ defmodule WaspVM.ExecutorTest do
       #assert stack_value == 4
       #assert stack_value2 == 4
     end
-
-
-
-
 end
