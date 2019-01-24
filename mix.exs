43739f89d6bb68fbc077a747b7ea44be7faf42ef
Alex Dovzhanyn <dovzhanyn.alex@gmail.com>
2019-01-24 10:55:43 -0500

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule WaspVM.MixProject do
   def project do
     [
       app: :wasp_vm,
-      version: "0.8.0",
+      version: "0.8.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
